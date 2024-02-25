4502cab5e3d50042175aefb115d06563614f1f12
tanishq <tanishq.malik31@gmail.com>
2024-02-25 18:32:55 +0530

    to add copy paste function , show in a box , typwriter animation

---
+++ script.js
@@ -0,0 +1,14 @@
+// function generatepass(){
+//     var length=12;
+//     var chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+//     var password=""
+
+//     for(i=0; i<length; i++){
+//         var randomindex= Math.floor(Math.random * chars.length)
+//         password=password+chars[randomindex]
+//     }
+
+//     document.getElementById("password").value = password
+// }
+
+
