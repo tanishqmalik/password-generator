94ef0c9247dd10d5d4fad5467a075ad14263114b
tanishq <tanishq.malik31@gmail.com>
2024-02-26 10:26:56 +0530

    -add copy paste feature

--- script.js
+++ script.js
@@ -12,3 +12,30 @@
 // }
 
 
+// var length=12;
+// var password=""
+
+
+// for(i=0; i<length; i++){
+//     let randomindex= Math.floor(Math.random() * mystring.length)
+//     password=password+mystring.charAt(randomindex)
+// }
+// // console.log(mystring.charAt(randomindex))
+// console.log(password)
+
+function generatepass(){
+    let mystring =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+    var length=15
+    var password=""
+
+    for(i=0; i<length; i++){
+        var randomindex= Math.floor(Math.random() * mystring.length)
+        password=password+mystring.charAt(randomindex)
+    }
+
+    document.getElementById("password").value = password
+}
+
+// let mystring =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+// var randomindex= Math.floor(Math.random()*mystring.length)
+// console.log(randomindex)
